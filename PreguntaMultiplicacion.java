108d4b3ab52109b0704a90809f8f89b836111a0c
Byron Montaño <byron.montano@unl.edu.ec>
2017-12-05 14:47:07 -0500

    Update PreguntaMultiplicacion.java

--- PreguntaMultiplicacion.java
+++ PreguntaMultiplicacion.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package TestdePreguntasInterface;
 
-/**
- *
- * @author DELL
- */
+
 public class PreguntaMultiplicacion implements PreguntaEntera{
     private int num1;
     private int num2;

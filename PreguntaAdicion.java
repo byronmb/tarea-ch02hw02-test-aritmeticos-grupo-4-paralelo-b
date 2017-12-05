ee83f3b38c1c6a705ed5f0bba8d98f0d52d4df06
Victor Serrano <victor.serrano@unl.edu.ec>
2017-12-04 19:14:12 -0500

    Update PreguntaAdicion.java

--- PreguntaAdicion.java
+++ PreguntaAdicion.java
@@ -12,30 +12,31 @@ import javax.swing.JOptionPane;
  * @author DELL
  */
 public class PreguntaAdicion implements PreguntaEntera{
+    //creamos las variables para las preguntas 
     private int num1;
     private int num2;
     private String lista;
     
-    public String getPregunta (){
+    public String getPregunta (){//Nos retorna la pregunta 
       return "Cuanto es "+num1+" + "+num2;    
     }
     
-    public int getRespuestaCorrecta(){
+    public int getRespuestaCorrecta(){//Retorna el resultado correcto de la pregunta
        return (num1+num2);
     }
 
-    public String compararRespuesta(int respuestaU){
-        if (respuestaU==getRespuestaCorrecta()) {
+    public String compararRespuesta(int respuestaU){//Compara si la respuesta dada por el usuario es correcta
+        if (respuestaU==getRespuestaCorrecta()) {//si la respuesta es correcta se guarda en la variable Lista la pregunta y la respuesta dada por el usuario
             //JOptionPane.showMessageDialog(null, getPregunta()+"="+getRespuestaCorrecta()+ " Correcta");
             lista=getPregunta()+"="+getRespuestaCorrecta()+ " Correcta";
-        }else{
+        }else{//Si la respuesta es incorrecta se guarda en la variable lista la pregunta con la respuesta del usuario
             //JOptionPane.showMessageDialog(null, getPregunta()+"="+getRespuestaCorrecta()+ " Tu respuesta: "+respuestaU+" Incorrecta");
             lista=getPregunta()+"="+getRespuestaCorrecta()+ "  Tu respuesta: "+respuestaU+" Incorrecta";
         }
         return lista;
     }
     
-    public PreguntaAdicion() {
+    public PreguntaAdicion() {//Se generan los numeros aleatorios para la pregunta
         this.num1 = (int) (Math.random()*50+1);
         this.num2 = (int) (Math.random()*50);
         
@@ -43,12 +44,12 @@ public class PreguntaAdicion implements PreguntaEntera{
 
     @Override
     public int getRespuestaCOrrecta() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); 
     }
 
     @Override
     public String compararRespuesta() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); 
     }
     
     
